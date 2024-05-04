129887a06e21429b68522ec09eba73321af5c6fc
navajyoth puthalath <106015664+navajyoth-puthalath13@users.noreply.github.com>
2024-05-04 19:25:12 +0530

    GUI interface design added

--- one_code.py
+++ one_code.py
@@ -5,7 +5,6 @@ import pyautogui
 import os
 import speech_recognition as sr
 import threading
-import sys
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5)
