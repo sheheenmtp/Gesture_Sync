06d7a2fba3e5cce7ba822bf4e645b74ed0069452
sheheenmtp <165380054+sheheenmtp@users.noreply.github.com>
2024-07-10 21:44:54 +0530

    Update mouse_control.py
    updated mouse code

--- mouse_control.py
+++ mouse_control.py
@@ -1,145 +1,83 @@
 import cv2
 import numpy as np
-import time
-import autopy
+import pyautogui
 import mediapipe as mp
 
+def mouse_control():
+    global m_analysis_active
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7, min_tracking_confidence=0.5)
 
-### HandDetector Class Definition ###
-class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=False, trackCon=0.5):
-        self.mode = mode
-        self.maxHands = maxHands
-        self.detectionCon = detectionCon
-        self.trackCon = trackCon
+    cap = cv2.VideoCapture(0)
+    m_analysis_active = True
 
-        self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
-                                        self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4, 8, 12, 16, 20]
+    smoothening = 5
+    frameR = 100  # Frame Reduction
+    prev_x, prev_y = 0, 0
+    curr_x, curr_y = 0, 0
 
-    def findHands(self, img, draw=True):    # Finds all hands in a frame
-        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        self.results = self.hands.process(imgRGB)
+    screen_width, screen_height = pyautogui.size()
 
-        if self.results.multi_hand_landmarks:
-            for handLms in self.results.multi_hand_landmarks:
-                if draw:
-                    self.mpDraw.draw_landmarks(img, handLms,
-                                               self.mpHands.HAND_CONNECTIONS)
+    while m_analysis_active and cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-        return img
+        frame = cv2.flip(frame, 1)  # Flip the frame
+        height, width, _ = frame.shape
 
-    def findPosition(self, img, handNo=0, draw=True):   # Fetches the position of hands
-        xList = []
-        yList = []
-        bbox = []
-        self.lmList = []
-        if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                xList.append(cx)
-                yList.append(cy)
-                self.lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(rgb_frame)
 
-            xmin, xmax = min(xList), max(xList)
-            ymin, ymax = min(yList), max(yList)
-            bbox = xmin, ymin, xmax, ymax
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                lmList = []
+                for id, lm in enumerate(hand_landmarks.landmark):
+                    cx, cy = int(lm.x * width), int(lm.y * height)
+                    lmList.append([id, cx, cy])
+                    cv2.circle(frame, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-            if draw:
-                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
-                              (0, 255, 0), 2)
+                if lmList:
+                    x1, y1 = lmList[8][1], lmList[8][2]  # Index finger tip
+                    x2, y2 = lmList[12][1], lmList[12][2]  # Middle finger tip
 
-        return self.lmList, bbox
+                    # Check which fingers are up
+                    fingers = []
+                    if lmList[8][2] < lmList[6][2]: fingers.append(1)  # Index finger
+                    else: fingers.append(0)
+                    if lmList[12][2] < lmList[10][2]: fingers.append(1)  # Middle finger
+                    else: fingers.append(0)
 
-    def fingersUp(self):    # Checks which fingers are up
-        fingers = []
-        # Thumb
-        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
-            fingers.append(1)
-        else:
-            fingers.append(0)
+                    # Only Index Finger: Moving Mode
+                    if fingers[0] == 1 and fingers[1] == 0:
+                        # Convert Coordinates
+                        x3 = np.interp(x1, (frameR, width - frameR), (0, screen_width))
+                        y3 = np.interp(y1, (frameR, height - frameR), (0, screen_height))
 
-        # Fingers
-        for id in range(1, 5):
+                        # Smoothen Values
+                        curr_x = prev_x + (x3 - prev_x) / smoothening
+                        curr_y = prev_y + (y3 - prev_y) / smoothening
 
-            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
-                fingers.append(1)
-            else:
-                fingers.append(0)
+                        # Move Mouse
+                        if abs(curr_x - prev_x) > 5 or abs(curr_y - prev_y) > 5:  # Minimum movement threshold
+                            pyautogui.moveTo(curr_x, curr_y)  # Remove the screen width flipping
+                        prev_x, prev_y = curr_x, curr_y
 
-        # totalFingers = fingers.count(1)
+                    # Both Index and Middle fingers are up: Clicking Mode
+                    if fingers[0] == 1 and fingers[1] == 1:
+                        # Find distance between fingers
+                        distance = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+                        if distance < 40:
+                            pyautogui.click()
 
-        return fingers
+                cv2.rectangle(frame, (frameR, frameR), (width - frameR, height - frameR), (255, 0, 255), 2)
 
-    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):   # Finds distance between two fingers
-        x1, y1 = self.lmList[p1][1:]
-        x2, y2 = self.lmList[p2][1:]
-        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        cv2.imshow("Mouse Control", frame)
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
 
-        if draw:
-            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-        length = np.hypot(x2 - x1, y2 - y1)
+    cap.release()
+    cv2.destroyAllWindows()
 
-        return length, img, [x1, y1, x2, y2, cx, cy]
-
-### Main Script ###
-
-pTime = 0               # Used to calculate frame rate
-width = 640             # Width of Camera
-height = 480            # Height of Camera
-frameR = 100            # Frame Rate
-smoothening = 8         # Smoothening Factor
-prev_x, prev_y = 0, 0   # Previous coordinates
-curr_x, curr_y = 0, 0   # Current coordinates
-
-cap = cv2.VideoCapture(0)   # Getting video feed from the webcam
-cap.set(3, width)           # Adjusting size
-cap.set(4, height)
-
-detector = handDetector(maxHands=1)                  # Detecting one hand at max
-screen_width, screen_height = autopy.screen.size()      # Getting the screen size
-
-while True:
-    success, img = cap.read()
-    img = detector.findHands(img)                       # Finding the hand
-    lmlist, bbox = detector.findPosition(img)           # Getting position of hand
-
-    if len(lmlist) != 0:
-        x1, y1 = lmlist[8][1:]
-        x2, y2 = lmlist[12][1:]
-
-        fingers = detector.fingersUp()      # Checking if fingers are upwards
-        cv2.rectangle(img, (frameR, frameR), (width - frameR, height - frameR), (255, 0, 255), 2)   # Creating boundary box
-        if fingers[1] == 1 and fingers[2] == 0:     # If fore finger is up and middle finger is down
-            x3 = np.interp(x1, (frameR, width - frameR), (0, screen_width))
-            y3 = np.interp(y1, (frameR, height - frameR), (0, screen_height))
-
-            curr_x = prev_x + (x3 - prev_x) / smoothening
-            curr_y = prev_y + (y3 - prev_y) / smoothening
-
-            autopy.mouse.move(screen_width - curr_x, curr_y)    # Moving the cursor
-            cv2.circle(img, (x1, y1), 7, (255, 0, 255), cv2.FILLED)
-            prev_x, prev_y = curr_x, curr_y
-
-        if fingers[1] == 1 and fingers[2] == 1:     # If fore finger & middle finger both are up
-            length, img, lineInfo = detector.findDistance(8, 12, img)
-
-            if length < 40:     # If both fingers are really close to each other
-                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
-                autopy.mouse.click()    # Perform Click
-
-    cTime = time.time()
-    fps = 1 / (cTime - pTime)
-    pTime = cTime
-    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+# Run the function
+mouse_control()
