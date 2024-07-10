c6b5ddeff55e5c7148ce80d1cbb7b74b0b14b7c3
sheheenmtp <165380054+sheheenmtp@users.noreply.github.com>
2024-07-10 21:43:46 +0530

    Update mouse_control.py
    mouse code added

--- mouse_control.py
+++ mouse_control.py
@@ -1,88 +1,145 @@
-import sys
-from tkinter import Tk, Canvas, Button, PhotoImage
-from pathlib import Path
-from threading import Thread
-from one_code import start_gesture_control
-from vol_control import volume_control, v_deactivate_analysis
-from open_control import hand_gesture_analysis, deactivate_analysis
-
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\mini_project\GUI\build\assets\frame0")
+import cv2
+import numpy as np
+import time
+import autopy
+import mediapipe as mp
 
 
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
+### HandDetector Class Definition ###
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectionCon=False, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
 
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
+                                        self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.tipIds = [4, 8, 12, 16, 20]
 
-def ter():
-    sys.exit()
+    def findHands(self, img, draw=True):    # Finds all hands in a frame
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
 
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,
+                                               self.mpHands.HAND_CONNECTIONS)
 
-def start_voice_thread():
-    voice_thread = Thread(target=start_gesture_control)
-    voice_thread.start()
+        return img
 
+    def findPosition(self, img, handNo=0, draw=True):   # Fetches the position of hands
+        xList = []
+        yList = []
+        bbox = []
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                xList.append(cx)
+                yList.append(cy)
+                self.lmList.append([id, cx, cy])
+                if draw:
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-def start_volume_thread():
-    volume_thread = Thread(target=volume_control)
-    volume_thread.start()
+            xmin, xmax = min(xList), max(xList)
+            ymin, ymax = min(yList), max(yList)
+            bbox = xmin, ymin, xmax, ymax
 
+            if draw:
+                cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
+                              (0, 255, 0), 2)
 
-def start_finger_thread():
-    finger_thread = Thread(target=hand_gesture_analysis)
-    finger_thread.start()
+        return self.lmList, bbox
 
+    def fingersUp(self):    # Checks which fingers are up
+        fingers = []
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
 
-def deactivate():
-    def wrapper():
-        deactivate_analysis()
-        v_deactivate_analysis()
+        # Fingers
+        for id in range(1, 5):
 
-    de_thread = Thread(target=wrapper)
-    de_thread.start()
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
 
-def close():
-    ter()
-    ter_thread = Thread(target=ter)
-    ter_thread.start()
+        # totalFingers = fingers.count(1)
 
-window = Tk()
-window.geometry("700x550")
-window.configure(bg="#FFFFFF")
+        return fingers
 
-canvas = Canvas(
-    window,
-    bg="#FFFFFF",
-    height=550,
-    width=700,
-    bd=0,
-    highlightthickness=0,
-    relief="ridge"
-)
-canvas.place(x=0, y=0)
+    def findDistance(self, p1, p2, img, draw=True,r=15, t=3):   # Finds distance between two fingers
+        x1, y1 = self.lmList[p1][1:]
+        x2, y2 = self.lmList[p2][1:]
+        cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
 
-# Load images
-button_image_1 = PhotoImage(file=relative_to_assets("button_1.png"))
-button_image_2 = PhotoImage(file=relative_to_assets("button_2.png"))
-button_image_3 = PhotoImage(file=relative_to_assets("button_3.png"))
-button_image_4 = PhotoImage(file=relative_to_assets("button_4.png"))
-button_image_5 = PhotoImage(file=relative_to_assets("button_5.png"))
-button_image_6 = PhotoImage(file=relative_to_assets("button_6.png"))
+        if draw:
+            cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
+            cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
+            cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
+        length = np.hypot(x2 - x1, y2 - y1)
 
-# Create buttons
-buttons = [
-    Button(image=button_image_1, borderwidth=0, highlightthickness=0, command=start_voice_thread, relief="flat"),
-    Button(image=button_image_2, borderwidth=0, highlightthickness=0, command=start_volume_thread, relief="flat"),
-    Button(image=button_image_3, borderwidth=0, highlightthickness=0, command=lambda: print("Button 3 clicked"), relief="flat"),
-    Button(image=button_image_4, borderwidth=0, highlightthickness=0, command=start_finger_thread, relief="flat"),
-    Button(image=button_image_5, borderwidth=0, highlightthickness=0, command=deactivate, relief="flat"),
-    Button(image=button_image_6, borderwidth=0, highlightthickness=0, command=ter, relief="flat")
-]
+        return length, img, [x1, y1, x2, y2, cx, cy]
 
-# Place buttons
-positions = [(577.0, 0.0), (71.0, 151.0), (270.0, 151.0), (469.0, 151.0), (191.0, 234.0), (400.0, 234.0)]
-for button, (x, y) in zip(buttons, positions):
-    button.place(x=x, y=y, width=button_image_1.width(), height=button_image_1.height())
+### Main Script ###
 
-window.resizable(False, False)
-window.mainloop()
+pTime = 0               # Used to calculate frame rate
+width = 640             # Width of Camera
+height = 480            # Height of Camera
+frameR = 100            # Frame Rate
+smoothening = 8         # Smoothening Factor
+prev_x, prev_y = 0, 0   # Previous coordinates
+curr_x, curr_y = 0, 0   # Current coordinates
+
+cap = cv2.VideoCapture(0)   # Getting video feed from the webcam
+cap.set(3, width)           # Adjusting size
+cap.set(4, height)
+
+detector = handDetector(maxHands=1)                  # Detecting one hand at max
+screen_width, screen_height = autopy.screen.size()      # Getting the screen size
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)                       # Finding the hand
+    lmlist, bbox = detector.findPosition(img)           # Getting position of hand
+
+    if len(lmlist) != 0:
+        x1, y1 = lmlist[8][1:]
+        x2, y2 = lmlist[12][1:]
+
+        fingers = detector.fingersUp()      # Checking if fingers are upwards
+        cv2.rectangle(img, (frameR, frameR), (width - frameR, height - frameR), (255, 0, 255), 2)   # Creating boundary box
+        if fingers[1] == 1 and fingers[2] == 0:     # If fore finger is up and middle finger is down
+            x3 = np.interp(x1, (frameR, width - frameR), (0, screen_width))
+            y3 = np.interp(y1, (frameR, height - frameR), (0, screen_height))
+
+            curr_x = prev_x + (x3 - prev_x) / smoothening
+            curr_y = prev_y + (y3 - prev_y) / smoothening
+
+            autopy.mouse.move(screen_width - curr_x, curr_y)    # Moving the cursor
+            cv2.circle(img, (x1, y1), 7, (255, 0, 255), cv2.FILLED)
+            prev_x, prev_y = curr_x, curr_y
+
+        if fingers[1] == 1 and fingers[2] == 1:     # If fore finger & middle finger both are up
+            length, img, lineInfo = detector.findDistance(8, 12, img)
+
+            if length < 40:     # If both fingers are really close to each other
+                cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
+                autopy.mouse.click()    # Perform Click
+
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    cv2.putText(img, str(int(fps)), (20, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
